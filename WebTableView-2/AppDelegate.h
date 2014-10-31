bcef31486678d2e2646f1fab6c45714cfbf530c2
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-31 09:15:01 -0700

    Initial Commit

---
+++ WebTableView-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebTableView-2
+//
+//  Created by Anthony Dagati on 10/31/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebTableView-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebTableView-2
+//
+//  Created by Anthony Dagati on 10/31/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
