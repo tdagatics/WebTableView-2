b397b9ba5bd3ef95b00e155a32ed2a616193b53e
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-31 09:22:36 -0700

    First commit

---
+++ WebTableView-2/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  WebTableView-2
+//
+//  Created by Anthony Dagati on 10/31/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController <UITableViewDataSource>
+
+@end
